460e80f732d444e703bef517aa9a32b2a74cd204
ImGorapaduck <han@certifie.io>
2023-09-05 18:47:58 +0900

    브랜치 변경 및 express 설치

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const serverless = require('serverless-http');
+
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+
+    res.send("Git Action Test Sccuess")
+
+})
+
+
+
+module.exports.handler = serverless(app);
